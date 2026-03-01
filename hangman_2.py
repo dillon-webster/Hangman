c49f99260a4b7ead1d73346185bd231118d6bf25
Dillon Webster <dillon.webster15@gmail.com>
2026-03-01 16:46:41 -0700

    fix merge conflict

--- hangman_2.py
+++ hangman_2.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from colorama import init, Fore, Back, Style
 import getpass
 import random
@@ -66,23 +66,6 @@ def computer_guess(freq: list[str], guessed: list[str]) -> str:
 
 
 
-=======
-import getpass
-import random
-
-
-def choose_word() -> str:
-    words: list[str] = [
-        "silever",
-        "diamond",
-        "emerald",
-        "gold",
-        "opal",
-
-    ]
-    return random.choice(words)
-    
->>>>>>> 7dad4677a2fee80a347b6f675bf0b4cdcfbd2543
 def display_hangman(tries: int) -> None:
     """ Displays the current stage oh hangman """
     stages: list[str] = [
@@ -163,9 +146,9 @@ def display_game(tries: int, word_completion: str) -> None:
     #Display the ______
     print(word_completion)
     print()
-<<<<<<< HEAD
 def play_hangman() -> None:
     game = Hangman()
+    guessed = False
     print()
     print("Welcome to Hangman!")
     print()
@@ -173,52 +156,10 @@ def play_hangman() -> None:
     display_game(game.tries, game.word_completion)
 
     while not guessed and game.tries > 0:
-=======
-
-def play_hangman() -> None:
-    print()
-    print("Welcome to Hangman!")
-    print()
-
-    #ask the user if they wna to input own word, or use a random one
-    #from the list
-    while True:
-        user_picks_word = input("Would you like to enter your own word? (y|n): ").lower()
-
-        if user_picks_word == "y" or user_picks_word == "n":
-            break
-
-        else:
-            print("Please enter 'y' or 'n'")
-    # IF the user is picking the word, get if from them now. Otherwise, pick
-    #a random one from our list. 
-
-    if user_picks_word =="y":
-        word = getpass.getpass("Enter the word to guess: ")
-    else:
-        word = choose_word()
-    # This will be the underscores showing the letters
-    word_completion = "_" *len(word)
-    
-    #this will be set to True if the word get successfully guess which will end the game
-    guessed = False
-
-    #keeps track of the letters being guessed, preventing duplicates
-    guessed_letters: list[str] = []
-
-    #Number of tries. This is used to determine which stage of the hangman process we are in
-    tries = 6
-
-    print("Let's play!")
-    display_game(tries, word_completion)
-
-    while not guessed and tries > 0:
->>>>>>> 7dad4677a2fee80a347b6f675bf0b4cdcfbd2543
         guess = input("Please guess a letter: ").lower()
 
         # Make sure they give a valid letter
         if len(guess) == 1 and guess.isalpha():
-<<<<<<< HEAD
             if guess in game.guessed_letters:
                 print(f"You already guessed the letter{guess}")
 
@@ -244,54 +185,18 @@ def play_hangman() -> None:
 
                 #Check if they've won
                 if "_" not in game.word_completion:
-=======
-            if guess in guessed_letters:
-                print(f"You already guessed the letter{guess}")
-
-            #wrong guess
-            elif guess not in word:
-                print(f"{guess} is not in the word.")
-
-                tries -= 1
-                guessed_letters.append(guess)
-
-            else:
-                print(f"Good job, {guess} is in the word!")
-
-                guessed_letters.append(guess)
-                
-                #make the _ _  _  _ thing
-                word_as_list: list[str] = list(word_completion)
-                indices: list[int] = [i for i, letter in enumerate(word) if letter == guess]
-                for index in indices:
-                    word_as_list[index] = guess
-                word_completion = "".join(word_as_list)
-
-                #Check if they've won
-                if "_" not in word_completion:
->>>>>>> 7dad4677a2fee80a347b6f675bf0b4cdcfbd2543
                     guessed = True
 
         else:
             print("Invalid input, please enter a single letter.")
 
-<<<<<<< HEAD
         display_game(game.tries, game.word_completion)
-=======
-        display_game(tries, word_completion)
->>>>>>> 7dad4677a2fee80a347b6f675bf0b4cdcfbd2543
 
     if guessed:
         print("Congratulations, you guessed the word! You win!")
 
     else:
-<<<<<<< HEAD
         print(f"Sorry, you ran out of tries. The word was {game.word} Maybe next time!.")
 
 computer_guess(frequency, [])
-=======
-        print(f"Sorry, you ran out of tries. The word was {word} Maybe next time!.")
-
-
->>>>>>> 7dad4677a2fee80a347b6f675bf0b4cdcfbd2543
 play_hangman()
