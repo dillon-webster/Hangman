1d9533546ce7df91508a4c51e12f884f825cfe42
Dillon Webster <dillon.webster15@gmail.com>
2026-03-03 18:28:35 -0700

    added user_guess function, gave more logic to computer

--- hangman_2.py
+++ hangman_2.py
@@ -8,7 +8,7 @@ import sys
 import time
 
 #pygame.mixer.init()
-#init(autoreset=True)
+init(autoreset=True)
 
 frequency = [
         "e",
@@ -53,12 +53,21 @@ class Hangman:
         self.word_completion = "_" * len(self.word)
         self.tries = 6
         self.guessed_letters = []
+def wait_times():
+    time.sleep(2)
+def computer_guess(freq: list[str], guessed: list[str], complete_word: str, secret_word: str) -> str:
+    #Computer automatically wins if only one letter left on it's turn
 
-def computer_guess(freq: list[str], guessed: list[str]) -> str:
+    if complete_word.count("_") == 1:
+        for letter, blank in zip(complete_word, secret_word):
+            if letter == "_":
+                return blank
     # Here is where the "computer" will detemine it's guess.
+
     # I splice the freq list so it will choose the most popular letters first
     sub_letters = freq[0:6]
     # If will use all the sub_letters first then move on to even_more_subbed
+
     even_more_subbed = freq[6:27]
     filtered1 = [letter for letter in sub_letters if letter not in guessed] 
     filtered2 = [letter for letter in even_more_subbed if letter not in guessed]
@@ -68,6 +77,19 @@ def computer_guess(freq: list[str], guessed: list[str]) -> str:
     else:  
         random_letter2 = random.choice(filtered2)
         return random_letter2
+
+def user_word_guess(correct_word: str, guess: str) -> bool:
+    if guess == correct_word:
+        print("Thats right! You win!")
+        sys.exit(0)
+        return True
+    else:
+        print("Sorry that is not right, you lose a life")
+        return False
+
+def clear_screen():
+    time.sleep(2)
+    os.system("cls" if os.name == "nt" else "clear")
 
 def user_vs_computer():
     game = Hangman()
@@ -80,61 +102,80 @@ def user_vs_computer():
     #computer_guessed = []
     print()
     print("Welcome to computer vs player Hangman!")
-    print("You will alternate guessing letters with the computer, first to guess the word wins?")
+    print("You will alternate guessing letters with the computer, first to guess the word wins!")
     print()
 
     # Player will go first.
     while not guessed and user_tries > 0 and computer_tries > 0:
-        print()
-        print("Player")
-        display_game(user_tries, game.word_completion)
-        guess = input("Please guess a letter: ").lower()
-        # Making sure it's the right guess
-        if len(guess) == 1 and guess.isalpha():
-                if guess in all_guessed:
-                    print(f"You already guessed the letter {guess}. Please try again.")
-                
-                # User wrong guess
-                elif guess not in game.word:
-                    print(f"{guess} is not in the word.")
+        valid_turn = True
+        if valid_turn == True:
+            print()
+            print(Fore.MAGENTA + "Player")
+            display_game(user_tries, game.word_completion)
+            # Seeing if player knows the word
+            choice1 = input("Do you think you know the word? y/n: ")
+            if choice1 == "y":
+                guess = input("Make your guess: ")
+                result = user_word_guess(game.word, guess)
+                #if player guessed right, game finished in user_guessed
+                #if player get it wrong, return False and loses life
+                #if player doesn't guess word, game moves on like normal
+                if result is False:
                     user_tries -= 1
-                    all_guessed.append(guess)
-                    print(all_guessed)
-                # User right Guess
-                elif guess in game.word:
-                    all_guessed.append(guess)
-                    print(all_guessed)
-                    #os.system("cls" if os.name == "nt" else "clear")
+                    valid_turn = False
+            if valid_turn == True:
+                print(all_guessed)
+                guess = input("Please guess a letter: ").lower()
+                # Making sure it's the right guess
+                if len(guess) == 1 and guess.isalpha():
+                        if guess in all_guessed:
+                            print(f"You already guessed the letter {guess}. Please try again.")
+                        
+                        # User wrong guess
+                        elif guess not in game.word:
+                            print(Fore.RED + f"{guess} is not in the word.")
+                            user_tries -= 1
+                            print(user_tries)
+                            all_guessed.append(guess)
+                            print(all_guessed)
+                        # User right Guess
+                        elif guess in game.word:
+                            all_guessed.append(guess)
+                            print(Fore.GREEN + f"{guess} is in the word!")
+                            print(all_guessed)  
 
-                    word_as_list = list(game.word_completion)
-                    indices: list[int] = [i for i, letter in enumerate(game.word) if letter == guess]
-                    for index in indices:
-                        word_as_list[index] = guess
-                    game.word_completion = "".join(word_as_list)
-                    #os.system("cls" if os.name == "nt" else "clear")
+                            word_as_list = list(game.word_completion)
+                            indices: list[int] = [i for i, letter in enumerate(game.word) if letter == guess]
+                            for index in indices:
+                                word_as_list[index] = guess
+                            game.word_completion = "".join(word_as_list)
 
-                    if "_" not in game.word_completion:
-                        current_player = "human"
-                        guessed = True
-                        break
+                            if "_" not in game.word_completion:
+                                current_player = "human"
+                                guessed = True
+                                break
+                            wait_times()
 
         #Computers turn
-        current_player = "computer" 
-        guessed_letter = computer_guess(frequency, all_guessed)
-        print()
-        print("Computer")
 
+        current_player = "computer" 
+        guessed_letter = computer_guess(frequency, all_guessed, game.word_completion, game.word)
+        print()
+        print(Fore.BLUE + "Computer")
         display_game(computer_tries, game.word_completion)
+        wait_times()
         print(f"Computers guess was {guessed_letter}.")
         # Computer wrong guess
         if guessed_letter not in game.word:
-            print(f"{guessed_letter} is not in the word.")
+            print(Fore.RED + f"{guessed_letter} is not in the word.")
+            wait_times()
             all_guessed.append(guessed_letter)
             print(all_guessed)
             computer_tries -= 1
+            print(computer_tries)
         # Computer right guess
         else:
-            print(f"{guessed_letter} is in the word!")
+            print(Fore.GREEN + f"{guessed_letter} is in the word!")
             all_guessed.append(guessed_letter)
             print(all_guessed)
             word_as_list = list(game.word_completion)
@@ -146,6 +187,7 @@ def user_vs_computer():
             if "_" not in game.word_completion:
                 guessed = True
                 break
+        clear_screen() 
 
     if current_player == "human" and guessed == True:                
         print("You win!")    
